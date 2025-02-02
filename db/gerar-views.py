5a303e49e83cec57499d6adbe8f35adda0a91f5c
Guilherme Fernandes <contatoguilhermelff@gmail.com>
2025-02-02 13:41:25 -0300

    oi

--- db/gerar-views.py
+++ db/gerar-views.py
@@ -1,14 +1,25 @@
 import pandas as pd
 
-CAMINHO_DB = "db/desempenho_funcionarios.csv"
+CAMINHO_DB = "db/view_vendas_desempenho_funcionarios.csv"
 
 df_db = pd.read_csv(CAMINHO_DB)
 
 def gerar_view(dataframe, coluna_filtro, valor_filtro):
     df_filtrado = dataframe[dataframe[coluna_filtro] == valor_filtro]
-    df_filtrado.to_csv(f"db/view_{valor_filtro}_{CAMINHO_DB.replace('db/', '')}", index=False)
+
+    colunas_para_remover = (df_filtrado == 0).all()
+
+    # Filtra as colunas para remover
+    colunas_para_remover = colunas_para_remover[colunas_para_remover].index
+
+    # Remove as colunas identificadas do DataFrame
+    df_filtrado = df_filtrado.drop(columns=colunas_para_remover)
+
+    df_filtrado.to_csv(f"db/{valor_filtro}_{CAMINHO_DB.replace('db/', '')}", index=False)
     return df_filtrado
 
-df_filtrado = gerar_view(df_db, "Departamento", "marketing")
+for i in ["junior", "pleno", "senior"]:
+
+    df_filtrado = gerar_view(df_db, "Cargo", i)
 
 
